6af6307088c4766d2b4ca9e37831c78ddf8874ba
PariKia <kiaeifar.pardis@gmail.com>
2023-07-27 18:51:58 +0330

    Update EmailSender.py

--- EmailSender.py
+++ EmailSender.py
@@ -1,37 +1,50 @@
 import smtplib
+import tkinter as tk
+from tkinter import messagebox
 
-gmail_user = input('Please enter your email:\n')
-gmail_password = input('Enter your password:\n')
+def send_email():
+    gmail_user = email_entry.get()
+    gmail_password = password_entry.get()
+    to_emails = to_entry.get().split(',')
+    subject = subject_entry.get()
+    body = body_text.get("1.0", "end")
 
-sent_from = gmail_user
-print('Enter the emails that you want to send email to them. Then, enter 0:')
-to = []
-while True:
-	email = input()
-	if email!='0':
-		to.append(email)
-	else:
-		break
+    email_text = f"""\
+From: {gmail_user}
+To: {", ".join(to_emails)}
+Subject: {subject}
 
-subject = input('Now, please enter the email subject:\n')
-body = input('Enter the body:\n')
+{body}
+"""
+# Create the main window
+window = tk.Tk()
+window.title("Email Sender")
+window.geometry("400x400")
+window.configure(bg="#f0f0f0")  # Set the background color
 
-email_text = """\
-From: %s
-To: %s
-Subject: %s
+# Create and place the widgets with colorful styling
+tk.Label(window, text="Email:", fg="blue", font=("Arial", 12, "bold"), bg="#f0f0f0").pack(pady=5)
+email_entry = tk.Entry(window, font=("Arial", 12))
+email_entry.pack()
 
-%s
-""" % (sent_from, ", ".join(to), subject, body)
+tk.Label(window, text="Password:", fg="blue", font=("Arial", 12, "bold"), bg="#f0f0f0").pack(pady=5)
+password_entry = tk.Entry(window, show="*", font=("Arial", 12))
+password_entry.pack()
 
-try:
-	smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-	smtp_server.ehlo()
-	smtp_server.login(gmail_user, gmail_password)
-	smtp_server.sendmail(sent_from, to, email_text)
-	smtp_server.close()
-	print ("Email sent successfully!")
-except Exception as ex:
-    print ("Something went wrong… check your google account, you should sign in to your gmail")
-    print('If not solved, check this link and enable the switch:\n')
-    print('https://www.google.com/settings/security/lesssecureapps')
+tk.Label(window, text="To (separate multiple emails with commas):", fg="blue", font=("Arial", 12, "bold"), bg="#f0f0f0").pack(pady=5)
+to_entry = tk.Entry(window, font=("Arial", 12))
+to_entry.pack()
+
+tk.Label(window, text="Subject:", fg="blue", font=("Arial", 12, "bold"), bg="#f0f0f0").pack(pady=5)
+subject_entry = tk.Entry(window, font=("Arial", 12))
+subject_entry.pack()
+
+tk.Label(window, text="Body:", fg="blue", font=("Arial", 12, "bold"), bg="#f0f0f0").pack(pady=5)
+body_text = tk.Text(window, width=40, height=10, font=("Arial", 12))
+body_text.pack()
+
+send_button = tk.Button(window, text="Send Email", command=send_email, bg="green", fg="white", font=("Arial", 12, "bold"))
+send_button.pack(pady=10)
+
+# Start the main event loop
+window.mainloop()
